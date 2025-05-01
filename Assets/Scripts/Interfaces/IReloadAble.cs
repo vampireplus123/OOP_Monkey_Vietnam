f006812b3f2fc2e301d3eb0a93ea24580da7eaab
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-05-01 14:03:42 +0700

    Interfaces created

---
+++ Assets/Scripts/Interfaces/IReloadAble.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+interface IReloadAble
+{
+    void Reload();
+}
---
+++ Assets/Scripts/Interfaces/IWeapons.cs
@@ -0,0 +1,7 @@
+interface Iweapons
+{  
+    void EquipWeapon();
+    void UnequipWeapon();
+    void Fire();
+    void Aim();
+}
